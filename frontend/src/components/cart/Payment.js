7aeae7ff09a33aae97014cf71e701c5b5fc2f7d9
Mehedi Khan Rakib <mkrakib@gmail.com>
2022-11-16 20:10:16 +0600

    latest update 2.0

--- frontend/src/components/cart/Payment.js
+++ frontend/src/components/cart/Payment.js
@@ -1,181 +1,214 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useEffect } from "react";
 
-import MetaData from '../layout/MetaData'
-import CheckoutSteps from './CheckoutSteps'
+import MetaData from "../layout/MetaData";
+import CheckoutSteps from "./CheckoutSteps";
 
-import { useAlert } from 'react-alert'
-import { useDispatch, useSelector } from 'react-redux'
-import { createOrder, clearErrors } from '../../actions/orderActions'
+import { useAlert } from "react-alert";
+import { useDispatch, useSelector } from "react-redux";
+import { createOrder, clearErrors } from "../../actions/orderActions";
 
-import { useStripe, useElements, CardNumberElement, CardExpiryElement, CardCvcElement } from '@stripe/react-stripe-js'
+import {
+        useStripe,
+        useElements,
+        CardNumberElement,
+        CardExpiryElement,
+        CardCvcElement,
+} from "@stripe/react-stripe-js";
 
-import axios from 'axios'
+import axios from "axios";
 
 const options = {
-    style: {
-        base: {
-            fontSize: '16px'
+        style: {
+                base: {
+                        fontSize: "16px",
+                },
+                invalid: {
+                        color: "#9e2146",
+                },
         },
-        invalid: {
-            color: '#9e2146'
-        }
-    }
-}
+};
 
 const Payment = ({ history }) => {
+        const alert = useAlert();
+        const stripe = useStripe();
+        const elements = useElements();
+        const dispatch = useDispatch();
 
-    const alert = useAlert();
-    const stripe = useStripe();
-    const elements = useElements();
-    const dispatch = useDispatch();
+        const { user } = useSelector((state) => state.auth);
+        const { cartItems, shippingInfo } = useSelector((state) => state.cart);
+        const { error } = useSelector((state) => state.newOrder);
 
-    const { user } = useSelector(state => state.auth)
-    const { cartItems, shippingInfo } = useSelector(state => state.cart);
-    const { error } = useSelector(state => state.newOrder)
+        useEffect(() => {
+                if (error) {
+                        alert.error(error);
+                        dispatch(clearErrors());
+                }
+        }, [dispatch, alert, error]);
 
-    useEffect(() => {
+        const order = {
+                orderItems: cartItems,
+                shippingInfo,
+        };
 
-        if (error) {
-            alert.error(error)
-            dispatch(clearErrors())
+        const orderInfo = JSON.parse(sessionStorage.getItem("orderInfo"));
+        if (orderInfo) {
+                order.itemsPrice = orderInfo.itemsPrice;
+                order.shippingPrice = orderInfo.shippingPrice;
+                order.taxPrice = orderInfo.taxPrice;
+                order.totalPrice = orderInfo.totalPrice;
         }
 
-    }, [dispatch, alert, error])
+        const paymentData = {
+                amount: Math.round(orderInfo.totalPrice * 100),
+        };
 
-    const order = {
-        orderItems: cartItems,
-        shippingInfo
-    }
+        const submitHandler = async (e) => {
+                e.preventDefault();
 
-    const orderInfo = JSON.parse(sessionStorage.getItem('orderInfo'));
-    if (orderInfo) {
-        order.itemsPrice = orderInfo.itemsPrice
-        order.shippingPrice = orderInfo.shippingPrice
-        order.taxPrice = orderInfo.taxPrice
-        order.totalPrice = orderInfo.totalPrice
-    }
+                document.querySelector("#pay_btn").disabled = true;
 
-    const paymentData = {
-        amount: Math.round(orderInfo.totalPrice * 100)
-    }
+                let res;
+                try {
+                        const config = {
+                                headers: {
+                                        "Content-Type": "application/json",
+                                },
+                        };
 
-    const submitHandler = async (e) => {
-        e.preventDefault();
+                        res = await axios.post(
+                                "/api/v1/payment/process",
+                                paymentData,
+                                config
+                        );
 
-        document.querySelector('#pay_btn').disabled = true;
+                        const clientSecret = res.data.client_secret;
 
-        let res;
-        try {
+                        console.log(clientSecret);
 
-            const config = {
-                headers: {
-                    'Content-Type': 'application/json'
+                        if (!stripe || !elements) {
+                                return;
+                        }
+
+                        const result = await stripe.confirmCardPayment(
+                                clientSecret,
+                                {
+                                        payment_method: {
+                                                card: elements.getElement(
+                                                        CardNumberElement
+                                                ),
+                                                billing_details: {
+                                                        name: user.name,
+                                                        email: user.email,
+                                                },
+                                        },
+                                }
+                        );
+
+                        if (result.error) {
+                                alert.error(result.error.message);
+                                document.querySelector(
+                                        "#pay_btn"
+                                ).disabled = false;
+                        } else {
+                                // The payment is processed or not
+                                if (
+                                        result.paymentIntent.status ===
+                                        "succeeded"
+                                ) {
+                                        order.paymentInfo = {
+                                                id: result.paymentIntent.id,
+                                                status: result.paymentIntent
+                                                        .status,
+                                        };
+
+                                        dispatch(createOrder(order));
+
+                                        history.push("/success");
+                                } else {
+                                        alert.error(
+                                                "There is some issue while payment processing"
+                                        );
+                                }
+                        }
+                } catch (error) {
+                        document.querySelector("#pay_btn").disabled = false;
+                        alert.error(error.response.data.message);
                 }
-            }
+        };
 
-            res = await axios.post('/api/v1/payment/process', paymentData, config)
+        return (
+                <Fragment>
+                        <MetaData title={"Payment"} />
 
-            const clientSecret = res.data.client_secret;
+                        <CheckoutSteps shipping confirmOrder payment />
 
-            console.log(clientSecret);
+                        <div className="row wrapper">
+                                <div className="col-10 col-lg-5">
+                                        <form
+                                                className="shadow-lg"
+                                                onSubmit={submitHandler}
+                                        >
+                                                <h1 className="mb-4">
+                                                        Card Info
+                                                </h1>
+                                                <div className="form-group">
+                                                        <label htmlFor="card_num_field">
+                                                                Card Number
+                                                        </label>
+                                                        <CardNumberElement
+                                                                type="text"
+                                                                id="card_num_field"
+                                                                className="form-control"
+                                                                options={
+                                                                        options
+                                                                }
+                                                        />
+                                                </div>
 
-            if (!stripe || !elements) {
-                return;
-            }
+                                                <div className="form-group">
+                                                        <label htmlFor="card_exp_field">
+                                                                Card Expiry
+                                                        </label>
+                                                        <CardExpiryElement
+                                                                type="text"
+                                                                id="card_exp_field"
+                                                                className="form-control"
+                                                                options={
+                                                                        options
+                                                                }
+                                                        />
+                                                </div>
 
-            const result = await stripe.confirmCardPayment(clientSecret, {
-                payment_method: {
-                    card: elements.getElement(CardNumberElement),
-                    billing_details: {
-                        name: user.name,
-                        email: user.email
-                    }
-                }
-            });
+                                                <div className="form-group">
+                                                        <label htmlFor="card_cvc_field">
+                                                                Card CVC
+                                                        </label>
+                                                        <CardCvcElement
+                                                                type="text"
+                                                                id="card_cvc_field"
+                                                                className="form-control"
+                                                                options={
+                                                                        options
+                                                                }
+                                                        />
+                                                </div>
 
-            if (result.error) {
-                alert.error(result.error.message);
-                document.querySelector('#pay_btn').disabled = false;
-            } else {
-
-                // The payment is processed or not
-                if (result.paymentIntent.status === 'succeeded') {
-
-                    order.paymentInfo = {
-                        id: result.paymentIntent.id,
-                        status: result.paymentIntent.status
-                    }
-
-                    dispatch(createOrder(order))
-
-                    history.push('/success')
-                } else {
-                    alert.error('There is some issue while payment processing')
-                }
-            }
-
-
-        } catch (error) {
-            document.querySelector('#pay_btn').disabled = false;
-            alert.error(error.response.data.message)
-        }
-    }
-
-    return (
-        <Fragment>
-            <MetaData title={'Payment'} />
-
-            <CheckoutSteps shipping confirmOrder payment />
-
-            <div className="row wrapper">
-                <div className="col-10 col-lg-5">
-                    <form className="shadow-lg" onSubmit={submitHandler}>
-                        <h1 className="mb-4">Card Info</h1>
-                        <div className="form-group">
-                            <label htmlFor="card_num_field">Card Number</label>
-                            <CardNumberElement
-                                type="text"
-                                id="card_num_field"
-                                className="form-control"
-                                options={options}
-                            />
+                                                <button
+                                                        id="pay_btn"
+                                                        type="submit"
+                                                        className="btn btn-block py-3"
+                                                >
+                                                        Pay{" "}
+                                                        {` - ${
+                                                                orderInfo &&
+                                                                orderInfo.totalPrice
+                                                        }`}
+                                                </button>
+                                        </form>
+                                </div>
                         </div>
+                </Fragment>
+        );
+};
 
-                        <div className="form-group">
-                            <label htmlFor="card_exp_field">Card Expiry</label>
-                            <CardExpiryElement
-                                type="text"
-                                id="card_exp_field"
-                                className="form-control"
-                                options={options}
-                            />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="card_cvc_field">Card CVC</label>
-                            <CardCvcElement
-                                type="text"
-                                id="card_cvc_field"
-                                className="form-control"
-                                options={options}
-                            />
-                        </div>
-
-
-                        <button
-                            id="pay_btn"
-                            type="submit"
-                            className="btn btn-block py-3"
-                        >
-                            Pay {` - ${orderInfo && orderInfo.totalPrice}`}
-                        </button>
-
-                    </form>
-                </div>
-            </div>
-
-        </Fragment>
-    )
-}
-
-export default Payment
+export default Payment;
